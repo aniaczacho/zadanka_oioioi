5d32d70da716d25d8f7e6d1f67b1a48941df9a51
aniaczacho <86191525+aniaczacho@users.noreply.github.com>
2021-06-20 14:39:48 +0200

    Update min.c

--- min.c
+++ min.c
@@ -1,39 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int max_i(int tab[], int n) {
-
-    int max_ = 0;
-
-    for (int i = 1; i < n; i++) {
-        if (tab[i] > tab[max_]) max_ = i;
-    }
-    return max_;
+int cmpfunc (const void * a, const void * b) {
+    return ( *(int*)a - *(int*)b );
 }
 
-
-
 int min_sum(int tab[], int n, int k) {
-
     for (int i = 0; i < k; i++) {
-        int m = max_i(tab, n);
-        tab[m] = (tab[m]/2);
+        tab[n - 1] = (tab[n - 1]/2);
+        int j = n - 1;
+        while ((j != 0) && (tab[j] < tab[j - 1])) {
+            int b = tab[j];
+            tab[j] = tab[j - 1];
+            tab[j - 1] = b;
+            j--;
+        }
     }
-
     int sum = 0;
     for (int i = 0; i < n; i++) sum += tab[i];
     return sum;
 }
 
-
 int main() {
 
     int n, k;
-
     scanf("%d%d", &n, &k);
 
     int tab[n];
-
     for (int i = 0; i < n; i++) scanf("%d", &tab[i]);
+
+    qsort(tab, n, sizeof(int), cmpfunc);
 
     printf("%d", min_sum(tab, n, k));
 
